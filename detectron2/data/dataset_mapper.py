7f22b3c18e9155f7c886654e5ab958d51dcab74d
Prathik0042 <prathikprats@gmail.com>
2025-04-24 05:40:43 +0530

    dataset_mapper skip added

--- detectron2/data/dataset_mapper.py
+++ detectron2/data/dataset_mapper.py
@@ -151,7 +151,12 @@ class DatasetMapper:
         """
         dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
         # USER: Write your own image loading if it's not from a file
-        image = utils.read_image(dataset_dict["file_name"], format=self.image_format)
+        try:
+            image = utils.read_image(dataset_dict["file_name"], format=self.image_format)
+            utils.check_image_size(dataset_dict, image)
+        except Exception as e:
+            print(f"Error loading image: {e}")
+            return dataset_dict
         utils.check_image_size(dataset_dict, image)
 
         # USER: Remove if you don't do semantic/panoptic segmentation.
